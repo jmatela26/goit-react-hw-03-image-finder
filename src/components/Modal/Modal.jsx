33dcc03db2830a300d9ab4195c2b6971282af3b4
jmatela26 <jmatela26@gmail.com>
2024-04-01 23:02:38 +0800

    Added fixes

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import styles from './Modal.module.css';
+import { Component } from 'react';
+
+export class Modal extends Component {
+  handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      this.props.closeModal();
+    }
+  };
+
+  handleKeyUp = (e) => {
+    if (e.key === 'Escape') {
+      this.props.closeModal();
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    const { isOpen, imageUrl } = this.props;
+    return (
+    <div className={isOpen ? styles.Overlay : styles.Hidden} onClick={this.handleOverlayClick}>
+      <div className={styles.Modal}>
+        <img src={imageUrl} alt="Large" />
+      </div>
+      </div>
+      )
+  }
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+};
